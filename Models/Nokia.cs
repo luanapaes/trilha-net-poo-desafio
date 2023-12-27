98ab467ad24f3d4977ce14b7b2ff766e37f3016f
Luana Paes <luanafranca598@gmail.com>
2023-12-26 22:49:09 -0300

    completando desafio

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
-namespace DesafioPOO.Models
+using System.ComponentModel;
+using DesafioPOO.Models;
+
+namespace Trilha_Net_Poo_Desafio.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia  : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) { }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} instalado no Nokia.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
+using Trilha_Net_Poo_Desafio.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Iphone
+Console.WriteLine("--- Iphone ---");
+Iphone i = new Iphone("998980000", "Iphone X", "123456", 128);
+
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("--------------------------------------");
+
+//Nokia
+Console.WriteLine("--- Nokia ---");
+Nokia n = new Nokia("921216776", "Nokia Lumia", "987654", 60);
+
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("Facebook");
